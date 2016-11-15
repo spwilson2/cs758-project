36a8472208e91e40822f94fb735d5e22b44a9de1
Ricky Mutschlechner <mutschlechne@wisc.edu>
2016-11-15 00:28:35 -0600

    blocking writes work, need to actually schedule them

--- scheduler-blocking/blocking.go
+++ scheduler-blocking/blocking.go
@@ -2,10 +2,40 @@ package blocking
 
 // Blocking IO Scheduler
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"syscall"
+)
 
 var TestExport int
 
-func main(){
-	fmt.Println("Hello world!")
+/* writes "Hello world!\n" to test.txt */
+func Write() {
+
+	// create if doesn't exist, and read/write
+	mode := syscall.O_CREAT | syscall.O_RDWR
+
+	// open file ./test.txt
+	fd, err := syscall.Open("./test.txt", mode, 0666)
+
+	if err != nil {
+		log.Fatal("File failed to open, exiting...")
+		os.Exit(1)
+	}
+
+	// write "Hello world!" to file
+	var p []byte
+	p = []byte("Hello world!\n")
+
+	n, err := syscall.Write(fd, p)
+
+	if err != nil {
+		log.Fatal("File failed to write, exiting...")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Bytes written: %d\n", n)
+
 }
