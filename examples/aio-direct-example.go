1beb368cb2bf0e8dac03d28ceed20e6ce448e1e4
Sean Wilson <spwilson27@gmail.com>
2016-11-24 01:35:35 -0600

    Add some more comments to examples, add a non direct aio example

--- examples/aio-direct-example.go
+++ examples/aio-direct-example.go
@@ -3,17 +3,17 @@ package main
 // package main must be the first thing in this program for the test to work.
 
 import "fmt"
+import "unsafe"
 import "os"
 import "errors"
 import "syscall"
 import aio "github.com/spwilson2/cs758-project/libaio"
 
-const VALID_STRING string = "package main"
-const TESTFILE string = "aio-example.go"
+const TESTFILE string = "aio-direct-example.go"
 
 var fail = errors.New("")
 
-const BUFSIZE = 5000000
+const BUFSIZE = 512
 
 func chk_err(err error) {
 	if err != nil {
@@ -27,32 +27,34 @@ func main() {
 	var ctx syscall.AioContext_t
 	var err error
 
-	// Create our AioContext with support for 1 inflight aio.
-	chk_err(syscall.IoSetup(1, &ctx))
-
+	chk_err(syscall.IoSetup(128, &ctx))
 	defer func() {
 		chk_err(syscall.IoDestroy(ctx))
 	}()
 
-	// Open our file using the systemcall
-	fd, err := syscall.Open(TESTFILE, syscall.O_RDONLY, 0)
+	fd, err := syscall.Open(TESTFILE, syscall.O_RDONLY|syscall.O_DIRECT, 0)
 	chk_err(err)
 
 	var iocb syscall.Iocb
 	var iocbp = &iocb
 
-	// Create our buffer for reading into
 	var buffer = make([]byte, BUFSIZE, BUFSIZE)
+
+	if (uintptr(unsafe.Pointer(&buffer[0])) & 0x01ff) > 0 {
+		fmt.Printf("Buffer must be 512 byte-aligned..\n")
+		fmt.Printf("0x%x\n", &buffer[0])
+		chk_err(fail)
+	}
+
 	aio.PrepPread(iocbp, fd, buffer, len(buffer), 0)
 
-	// Submit our request for AIO.
+	// Submit our request.
 	chk_err(syscall.IoSubmit(ctx, 1, &iocbp))
 
 	var event syscall.IoEvent
 	var timeout syscall.Timespec
 	timeout.Sec = 1
 
-	// Get back the result from our AIO call.
 	events := syscall.IoGetevents(ctx, 1, 1, &event, &timeout)
 	if events == 0 {
 		chk_err(fail)
@@ -61,8 +63,10 @@ func main() {
 	}
 
 	// Check the result..
-	if string(buffer[:len(VALID_STRING)]) != VALID_STRING {
-		fmt.Printf("Expected:%s, Found:%s\n", VALID_STRING, buffer)
+	valid_string := "package main"
+
+	if string(buffer[:len(valid_string)]) != valid_string {
+		fmt.Printf("Expected:%s, Found:%s\n", valid_string, buffer)
 		chk_err(fail)
 	}
 
