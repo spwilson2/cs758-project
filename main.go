a6f52142092d75f416c93111749836382e807b4e
Michael Cook <mjcook3@wisc.edu>
2016-11-27 20:29:38 -0600

    compiles and removed interactive elements.

--- main.go
+++ main.go
@@ -3,47 +3,10 @@ package main
 import (
 	"log"
 	"time"
-	"os"
-	"bufio"
-	"strconv"
-
-	blocking "github.com/spwilson2/cs758-project/scheduler-blocking"
-	nonblocking "github.com/spwilson2/cs758-project/scheduler-nonblocking"
 )
 
-// We test 1 KB, 10 KB, 100 KB, 1MB, and 10 MB (5 steps of a log scale) 
-sizesPerTest := [5]{1000, 10000, 100000, 1000000, 10000000}
-
 func main() {
-	if len(os.Args) == 2 && os.Args[1] == "--default" {
-		performAllBenchmarks()
-		return
-	}
-
-	usagePrompt()
-}
-
-func usagePrompt() (int) {
-	fmt.Println("***************************************************************************")
-	fmt.Println("** Golang Blocking and Asynchonous Scheduling Simulation Benmarking tool **")
-	fmt.Println("***************************************************************************\n")
-
-	fmt.Println("Testing Options")
-	fmt.Println("1) All Benchmarks")
-	fmt.Println("2) BIO Benchmarks")
-	fmt.Println("3) AIO Benchmarks")
-	
-	fmt.Print("Benchmark option: ")
-
-	return getTestOption()
-}
-
-func getTestOption() (int) {
-	reader := bufio.NewReader(os.Stdin)
-	option, _ := reader.ReadString('\n')
-	
-	optionInt, _ := strconv.Atoi(option)
-	return optionInt
+	performAllBenchmarks()
 }
 
 // Benchmark option 1
@@ -69,60 +32,60 @@ func performAsyncBenchmarks() {
 }
 
 func performSequentialBlockingWriteBenchmarks() {
-
+	defer un(trace("SBW"))
 }
 
 func performSequentialBlockingReadBenchmarks() {
-
+	defer un(trace("SBR"))
 }
 
 func performSequentialAsyncWriteBenchmarks() {
-
+	defer un(trace("SAW"))
 }
 
 func performSequentialAsyncReadBenchmarks() {
-
+	defer un(trace("SAR"))
 }
 
 func performRandomBlockingWriteBenchmarks() {
-
+	defer un(trace("RBW"))
 }
 
 func performRandomBlockingReadBenchmarks() {
-
+	defer un(trace("RBR"))
 }
 
 func performRandomAsyncWriteBenchmarks() {
-
+	defer un(trace("RAW"))
 }
 
 func performRandomAsyncReadBenchmarks() {
-
+	defer un(trace("RAR"))
 }
 
-/* 
+/*
 	These functions can be used together to benchmark a functions. Simply provide
 	a unique identifier that is used for logging when calling the functions using
 	the defer keyword:
 
 	e.g. defer un(trace("testId"))
 
-	Basically, the nested function trace() is called immediately and that grabs 
+	Basically, the nested function trace() is called immediately and that grabs
 	the start time which is passed to un(); however, un() is not executed until
 	the end of the function. This Is pretty clean, but you'll need a seperate
 	function that must start with this and continue no code beyond what you
 	want to benchmark.
- */ 
+*/
 
-func trace(id string) (string, Time) {
+func trace(id string) (string, time.Time) {
 	log.Printf("Benchmarking: %s", id)
 	start := time.Now()
-	return (id, start)
+	return id, start
 }
 
-func un(id string, Time start) (Duration) {
-	elapsed := time.Since(time)
-	log.Printf("%s completed in %d", id, elapsed.Nanoseconds())
+func un(id string, start time.Time) time.Duration {
+	elapsed := time.Since(start)
+	log.Printf("%s completed in %d nanoseconds\n\n", id, elapsed.Nanoseconds())
 	return elapsed
 }
 
