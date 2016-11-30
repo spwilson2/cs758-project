82487a5908205bbbc8fbf5151795390ad4046a7f
Michael Cook <mjcook3@wisc.edu>
2016-11-30 04:58:18 -0600

    generated graphs for data collected ale

--- generateGraph.R
+++ generateGraph.R
@@ -9,4 +9,4 @@ graphName <- strsplit(strsplit(args[1],"\\.")[[1]][2], "\\/")[[1]][3]
 png(file = paste("graphs/", graphName, ".png", sep=""))
 
 #barplot(csvAsMatrx)
-barplot(csvAsMatrx, xlab="operation Sizes", ylab="time (ns)", main=graphName, names.arg=c("1 KB", "10 KB", "100 KB", "1 MB"))
+barplot(csvAsMatrx, xlab="operation Sizes", ylab="time (ns)", main=graphName, names.arg=c("1 KB", "10 KB", "100 KB"))
