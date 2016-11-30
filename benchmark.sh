83729f214d66ab2494f6b4acd4f355fd87e36d68
Michael Cook <mjcook3@wisc.edu>
2016-11-30 01:35:06 -0600

    fixed the script

--- benchmark.sh
+++ benchmark.sh
@@ -4,22 +4,21 @@ runBenchmark() {
     do
         writeSum=0
         readSum=0
-        for j in 1..10
+        for j in `seq 1 10`;
         do
             echo "Running $1 benchmark with $i KB writes and reads"
             output=$(./main -$2 -$3 -size $(($i * 1000)))
             
             writeResult=$(echo "$output" | head -n 1)
-            writeSum+=$writeResult
-            
-            readResult=$(echo "$output" | tail -1)
-            readSum+=$readResult
+            let writeSum=writeSum+$writeResult
 
-            if [ j != 1 ]; then 
+            readResult=$(echo "$output" | tail -1)
+            let readSum=readSum+$readResult
+
+            if [ $j -eq 1 ] ; then 
                 printf "%d" $(($writeSum/10)) >> $2.csv
                 printf "%d" $(($readSum/10)) >> $3.csv
             else
-                echo why u not run
                 printf ",%d" $(($writeSum/10)) >> $2.csv
                 printf ",%d" $(($readSum/10)) >> $3.csv
             fi
